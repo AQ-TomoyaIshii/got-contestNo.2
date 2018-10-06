d9dece4898cfb470f3c70758578420f5653af704
もや <glhfty0066@gmail.com>
2018-10-06 14:02:51 +0900

    server-side weather & deaplearning
    new data fetch

--- py_server/OpenweatherMap.py
+++ py_server/OpenweatherMap.py
@@ -1,20 +1,44 @@
-import requests
 import json
+import requests
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+apikey = '15f84a85eafc4d84daef224a808ff0e4'
+cityName = "Yokohama"
+apiUrl = 'http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&APPID={key}'
+apiUrl2 = "http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={key}"
 
-apikey = "15f84a85eafc4d84daef224a808ff0e4"
-cities = ["Tokyo,JP", "Kanagawa", "Yokohama"]
-api = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key)"
+# Flaskクラスのインスタンスを生成
+#__name__は現在のファイルのモジュール名
+api = Flask(__name__)
+CORS(api)
 
-k2c = lambda k: k - 273.15
-
-for name in cities:
-    url = api.format(city=name, key=apikey)
+@api.route('/<cityName>',methods=['GET'])
+def get(cityName):
+    url = apiUrl.format(city=cityName, key=apikey)
     r = requests.get(url)
     data = json.loads(r.text)
-    print("+ 都市=", data["name"])
-    print("| 天気=", data["weather"][0]["description"])
-    print("| 最低気温=", k2c(data["main"]["temp_min"]))
-    print("| 現在気温=", k2c(data["main"]["temp"]))
-    print("| 最高気温=", k2c(data["main"]["temp_max"]))
-    print("| 湿度=", data["main"]["humidity"])
-    print("")
+
+    result = {
+    "unko":data
+    }
+    return make_response(jsonify(result))
+
+
+
+
+
+
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify(['error: Not found']), 404)
+
+#ファイルをスクリプトとして実行した際に
+#ホスト0,0,0,0,ポート3001番でサーバーを起動
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=3001)
+
+
+
+
+
+
